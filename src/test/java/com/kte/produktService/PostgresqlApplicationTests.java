df682072a88867b3dca2574a66388d1dc2ff549c
ledemkam <ledemkam@gmail>
2024-10-14 10:21:50 +0200

    projet init

---
+++ src/test/java/com/kte/produktService/PostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kte.produktService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
